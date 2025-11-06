bf5b8e24aab5cb2e4cf1a7516a62ecccc9799e00
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-11-06 21:41:43 +0100

    Create hello world client

---
+++ hello-world-client/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  
+  import { onMount } from "svelte";
+  import { getGreeting } from "./lib/api.js";
+
+  let greeting = "Loading...";
+
+  onMount(async () => {
+    try {
+      greeting = await getGreeting();
+    } catch (err) {
+      greeting = "Error fetching greeting 😢";
+      console.error(err);
+    }
+  });
+</script>
+
+<main>
+  <h1>{greeting}</h1>
+</main>
---
+++ hello-world-client/src/lib/api.js
@@ -0,0 +1,6 @@
+export async function getGreeting() {
+  const res = await fetch("/api/greeting"); // no localhost or port!
+  if (!res.ok) throw new Error("Failed to fetch greeting");
+  const data = await res.json();
+  return data.message;
+}
